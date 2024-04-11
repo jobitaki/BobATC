4df0f51b1ea08e5e29071f66d3cc4e55ce36cd57
Jaehyun Lim <jaehyunrlim@gmail.com>
2024-04-11 00:39:43 -0400

    Fixed bug where unlock actually locks runway

--- Bob.sv
+++ Bob.sv
@@ -539,7 +539,7 @@ module RunwayManager
             runway[1].active <= 1'b0;
         end else begin
           if (plane_id == runway[0].plane_id)
-            runway[1].active <= 1'b1;
+            runway[1].active <= 1'b0;
         end
      end
     end
