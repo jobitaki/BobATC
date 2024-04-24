bb05ac4ce6409a2d4c02c4c32c485d928a393ab9
Jaehyun Lim <jaehyunrlim@gmail.com>
2024-04-24 13:23:34 -0400

    Revised testbench, changed package order

--- test/Bob_test.py
+++ test/Bob_test.py
@@ -33,7 +33,7 @@ async def send_uart_request(dut, data):
 async def basic_test(dut):
   print("Begin basic takeoff request")
 
-  rand_id = (random.randint(0, 15) << 5)
+  
 
   # Run the clock
   cocotb.start_soon(Clock(dut.clock, 100, units="ns").start())
@@ -44,16 +44,70 @@ async def basic_test(dut):
   dut.reset_n.value = True
 
   dut.uart_tx_ready.value = True
-  await send_uart_request(dut, rand_id + T_REQUEST + R_TAKEOFF)
+
+  # Plane id_1 requests takeoff
+  id_1 = (random.randint(0, 15) << 5)
+  await send_uart_request(dut, id_1 + T_REQUEST + R_TAKEOFF)
   start = get_sim_time(units="ns")
+  print(f"Plane {id_1 >> 5}: Requesting takeoff at time {start}")
+
   while True:
     await FallingEdge(dut.clock)
     if get_sim_time(units="ns") - start > 10000:
       raise TimeoutError("Did not receive clearance")
-    if dut.uart_tx_data.value == rand_id + T_CLEAR + C_RUNWAY_0: # Clear takeoff 0
-      print(f"Received clearance for takeoff ID 5 at time {get_sim_time(units="ns")}")
+    if (dut.uart_tx_data.value & 0b111111100) == id_1 + T_CLEAR: # Clear takeoff
+      end = get_sim_time(units="ns")
+      print(f"Bob: Plane {id_1 >> 5} cleared to take off runway {dut.uart_tx_data.value & 0b000000011} at time {end}")
       break
   await FallingEdge(dut.clock)
-  await send_uart_request(dut, 0b010100100)
-  for i in range(100):
+
+  # Plane id_2 requests landing
+  id_2 = (random.randint(0, 15) << 5)
+  await send_uart_request(dut, id_2 + T_REQUEST + R_LANDING)
+  start = get_sim_time(units = "ns")
+  print(f"Plane {id_2 >> 5}: Requesting landing at time {start}")
+  
+  while True:
     await FallingEdge(dut.clock)
+    if get_sim_time(units="ns") - start > 10000:
+      raise TimeoutError("Did not receive clearance")
+    if (dut.uart_tx_data.value & 0b111111100) == id_2 + T_CLEAR: # Clear landing
+      end = get_sim_time(units="ns")
+      print(f"Bob: Plane {id_2 >> 5} cleared to land runway {dut.uart_tx_data.value & 0b000000011} at time {end}")
+      break
+  await FallingEdge(dut.clock)
+
+  # Plane id_3 reqeusts takeoff
+  id_3 = (random.randint(0, 15) << 5)
+  await send_uart_request(dut, id_3 + T_REQUEST + R_TAKEOFF)
+  start = get_sim_time(units="ns")
+  print(f"Plane {id_3 >> 5}: Requesting takeoff at time {start}")
+
+  while True:
+    await FallingEdge(dut.clock)
+    if get_sim_time(units="ns") - start > 10000:
+      raise TimeoutError("Did not receive clearance")
+    if (dut.uart_tx_data.value & 0b111111100) == id_3 + T_HOLD: # Hold
+      end = get_sim_time(units="ns")
+      print(f"Bob: Plane {id_3 >> 5} hold at time {end}")
+      break
+  await FallingEdge(dut.clock)
+
+  # Plane id_1 declares takeoff runway 0
+  await send_uart_request(dut, id_1 + T_DECLARE + D_TAKEOFF_0)
+  start = get_sim_time(units="ns")
+  print(f"Plane {id_1 >> 5}: Declaring takeoff on runway 0 at time {start}")
+
+  # Await clearance of plane id_3 on now free runway
+  while True:
+    await FallingEdge(dut.clock)
+    if get_sim_time(units="ns") - start > 10000:
+      raise TimeoutError("Did not receive clearance")
+    if (dut.uart_tx_data.value & 0b111111100) == id_3 + T_CLEAR: # Clear takeoff
+      end = get_sim_time(units="ns")
+      print(f"Bob: Plane {id_3 >> 5} clear to takeoff runway {dut.uart_tx_data.value & 0b000000011} at time {end}")
+      break
+  await FallingEdge(dut.clock)
+
+  await FallingEdge(dut.clock)
+  await FallingEdge(dut.clock)
